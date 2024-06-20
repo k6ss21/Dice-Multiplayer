d84d2e0cf4b8dd0e6bcc0cb94d4da494c9e020f2
k6ss21 <krishnaramss21@gmail.com>
2024-06-20 18:34:33 +0530

    Some UI Changes

---
+++ Assets/Scripts/SkillDetails.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Skills/SkillDetails")]
+public class SkillDetails : ScriptableObject
+{
+   public string skillCode ;
+   public string skillName;
+
+   
+}
